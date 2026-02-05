e198868ea1c5d2677c898c6d89c7ca41a6a25b93
miloss98 <stojanovic98m@gmail.com>
2026-02-05 01:23:53 +0100

    removed unused font, Roboto only one now

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Roboto } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ScrollHandler, ScrollToTop } from "./components";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -34,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${roboto.variable} ${geistSans.variable} ${geistMono.variable}  antialiased`}
-      >
+      <body className={`${roboto.variable} antialiased`}>
         <ScrollHandler />
         <ScrollToTop />
         {children}
